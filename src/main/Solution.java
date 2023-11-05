e66c9adcb98080e5808664ad95d7b8b2185e5568
li <a374859311@gmail.com>
2023-11-05 10:24:12 +0800

    inital

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -7,24 +7,5 @@ import java.util.*;
 
 
 class Solution {
-    int[] ans = new int[38];
 
-    public int tribonacci(int n) {
-        if(n == 0) return 0;
-        else if(n == 1) return 1;
-        else if(n == 2) return 1;
-        else{
-            ans[0] = 0;
-            ans[1] = 1;
-            ans[2] = 1;
-        }
-        return dfs(ans,n);
-    }
-    int dfs(int[] ans,int n){
-        if(ans[n]!=0) return ans[n];
-        else {
-            ans[n] = dfs(ans,n-1)+dfs(ans,n-2)+dfs(ans,n-3);
-        }
-        return ans[n];
-    }
 }
